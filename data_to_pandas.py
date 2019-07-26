1407cb8fd5bd50dcf32d3683705f4a07cc573d49
Abdullah Khilji <abdullahkhilji.nits@gmail.com>
2019-07-26 18:02:04 +0530

    Data Frame with probabilities.

--- data_to_pandas.py
+++ data_to_pandas.py
@@ -12,24 +12,24 @@ if ".DS_Store" in labels:
 all_data = []
 
 # uncomment to debug
-# labels = labels[:2]
-# counter = 0
+labels = labels[:5]
+counter = 0
 
 for i in labels:
     instances_in_a_label = os.listdir ("Dataset/ohsumed-all/" + i)
     all_data_for_a_label = []
     for j in instances_in_a_label:
         # uncomment to debug
-        # if counter < 3:
-        f = open("Dataset/ohsumed-all/" + i + '/' + j, "r")
-        raw_data = f.read()
-        preprocessed_data = re.sub('[^a-zA-Z]', ' ', raw_data).lower()
-        preprocessed_data = preprocessed_data.split()
-        preprocessed_data = [word for word in preprocessed_data if word not in stopwords.words('english')]
-        preprocessed_data = ' '.join(preprocessed_data)
-        all_data.append([preprocessed_data, i])
-    #     counter += 1
-    # counter = 0
+        if counter < 2:
+            f = open("Dataset/ohsumed-all/" + i + '/' + j, "r")
+            raw_data = f.read()
+            preprocessed_data = re.sub('[^a-zA-Z]', ' ', raw_data).lower()
+            preprocessed_data = preprocessed_data.split()
+            preprocessed_data = [word for word in preprocessed_data if word not in stopwords.words('english')]
+            preprocessed_data = ' '.join(preprocessed_data)
+            all_data.append([preprocessed_data, i])
+            counter += 1
+    counter = 0
 
 all_data = np.asarray(all_data)
 df = pd.DataFrame(all_data)
