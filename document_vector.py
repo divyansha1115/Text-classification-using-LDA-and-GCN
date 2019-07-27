0cf8b20768872358c34f5fde1792367af30ca014
Abdullah Khilji <abdullahkhilji.nits@gmail.com>
2019-07-27 16:26:55 +0530

    Word2Vec Document Vectors

---
+++ document_vector.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+# coding: utf-8
+
+# In[1]:
+
+import gensim
+
+import numpy as np
+
+from nltk import word_tokenize
+
+
+model = gensim.models.KeyedVectors.load_word2vec_format('word2vec/vectors_small.txt', binary=False)
+
+df = pd.read_csv('pre_processed_data.csv')
+df = list(df['0'])
+
+
+def document_vector(word2vec_model, doc):
+    # remove out-of-vocabulary words
+    doc = [word for word in doc if word in word2vec_model.vocab]
+    return np.mean(word2vec_model[doc], axis=0)
+
+def preprocess(text):
+    doc = word_tokenize(text)
+    return doc
+
+corpus = [preprocess(text) for text in df]
+
+x =[]
+for doc in df: #look up each doc in model
+    x.append(document_vector(model, doc))
+
+x = np.asarray(x)
+x = pd.DataFrame(x)
+
+df = np.asarray(df)
+df = pd.DataFrame(df)
+
+final_data = pd.concat([df, x], axis=1)
+
+final_data.to_csv('w2v.csv')
--- w2v/w2v.sh
+++
@@ -1,3 +0,0 @@
-make
-time ./word2vec -train text8 -output vectors.txt -cbow 1 -size 50 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 0 -iter 15
-./distance vectors.txt
