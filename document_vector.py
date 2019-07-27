38c499ce292539bcb1dcfaf0011919f1bcde2036
Abdullah Khilji <abdullahkhilji.nits@gmail.com>
2019-07-27 16:29:50 +0530

    Added labels.

--- document_vector.py
+++ document_vector.py
@@ -14,6 +14,7 @@ from nltk import word_tokenize
 model = gensim.models.KeyedVectors.load_word2vec_format('word2vec/vectors_small.txt', binary=False)
 
 df = pd.read_csv('pre_processed_data.csv')
+orig_df = df
 df = list(df['0'])
 
 
@@ -38,6 +39,6 @@ x = pd.DataFrame(x)
 df = np.asarray(df)
 df = pd.DataFrame(df)
 
-final_data = pd.concat([df, x], axis=1)
+final_data = pd.concat([orig_df, x], axis=1)
 
 final_data.to_csv('w2v.csv')
